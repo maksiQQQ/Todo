91965539428c9fc62d703e994a2383579bc973b4
maksiQQQ <maksiqkgz@gmail.com>
2021-11-06 00:11:45 +0600

    added model ToMeet

--- Desktop/Visual studio code/ToDoOriginal/main/models.py
+++ Desktop/Visual studio code/ToDoOriginal/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    persone = models.TextField(max_length=50)
+    phone_number = models.IntegerField()
+    date_of_meeting = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
